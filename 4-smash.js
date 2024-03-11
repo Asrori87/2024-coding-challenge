cb6b6319c223dc04ed0f24acf9f41dca81653181
fauzanasrori <m45120121@gmail.com>
2024-03-11 14:10:25 +0700

    github test

---
+++ 4-smash.js
@@ -0,0 +1,12 @@
+// WPU Codding Challenge 2024
+// 3/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+function smash(words) {
+  return words;
+}
+
+console.log(
+  smash(["this", "is", "a", "really", "long", "sentence"]),
+  "this is a really long sentence"
+);
