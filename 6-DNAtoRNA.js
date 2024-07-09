f6f54cd289440a985793f2aa3268cbe496ad2c0e
Asrori <fauzanasrori87@gmail.com>
2024-07-09 19:07:43 +0700

    6/336

---
+++ 6-DNAtoRNA.js
@@ -0,0 +1,23 @@
+// WPU Coding Challenge 2024
+// 6/366
+// https://www.codewars.com/kata/5556282156230d0e5e000089
+
+// 1. versi barbar
+function DNAtoRNA(dna) {
+  let rna = "";
+
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "T") rna += "U";
+    else rna += dna[i];
+  }
+
+  return rna;
+}
+
+// function DNAtoRNA(dna) {
+//   // return dna.split('T').join('U'); // using Array.prototype.join() for efficiency and conciseness.
+//   // return dna.replace(/T/g, 'U'); // using regular expression for efficiency and conciseness.
+//   return dna.length > 0 ? dna : "Return kosong";
+// }
+
+console.log(DNAtoRNA("GCAT"));
