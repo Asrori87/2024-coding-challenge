bdc408fc3dcb6d95d555e3ea4c4bb20d19971385
Asrori <fauzanasrori87@gmail.com>
2024-07-09 20:46:56 +0700

    day-7

---
+++ 7-countBy.js
@@ -0,0 +1,20 @@
+// cara barbar
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 0; i < n; i++) {
+//     z.push(x * (i + 1));
+//   }
+
+//   return z;
+// }
+
+// cara elegant
+function countBy(x, n) {
+  // ++i == (i + 1)
+  return [...Array(n)].map((_, i) => ++i * x);
+}
+
+console.log(countBy(2, 5));
+
+// countBy(1,10), [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5), [2,4,6,8,10]
