d77c350e236e3612ad8762b163ea92a4e8f1ebdb
Asrori <fauzanasrori87@gmail.com>
2024-07-10 08:53:33 +0700

    day 10/365

---
+++ 10-stringToArray.js
@@ -0,0 +1,14 @@
+// WPU Codding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  // code code code
+  return string.split(" ");
+}
+
+console.log(stringToArray(""));
+
+// Examples
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
